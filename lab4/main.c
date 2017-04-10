8298197a8da69bd0846aa0ad9bdf74486dc5919e
Yura Kim <yuraxkim@gmail.com>
2017-04-09 21:07:46 -0400

    clean up for submission

--- lab4/main.c
+++ lab4/main.c
@@ -11,10 +11,9 @@
 #include "customer_queue.h"
 #include "timer.h"
 
-#define NUM_TELLERS 3
 #define NUM_SECONDS_IN_DAY 25200
-#define US_IN_SECOND 100
-#define MAX_CUSTOMERS_IN_DAY 254
+#define TIMER_INTERVAL_IN_NSEC 100
+#define TIMER_COUNT 10
 
 int current_time;
 
@@ -29,18 +28,17 @@ int max_teller_wait = 0;
 int max_transaction_time = 0;
 int number_of_teller_waits = 0;
 
+// Customer queue resources
+Customer *cust_gen[MAX_CUSTOMERS_IN_DAY];
 pthread_mutex_t ml = PTHREAD_MUTEX_INITIALIZER;
 
-int threads_running = 4;
-Customer *cust_gen[MAX_CUSTOMERS_IN_DAY];
-
-
-// Teller threads
+// Define threads
 pthread_t teller1;
 pthread_t teller2;
 pthread_t teller3;
 pthread_t customer_queue_manager;
 pthread_t timer_thread;
+int threads_running = 4;
 
 
 /*
@@ -64,14 +62,13 @@ void printMetrics() {
 void *teller_runner(void *id) {
 	long tid;
 	tid = (long)id;
-	printf("Teller thread %ld started!\n", tid);
 	int waitStart = 0;
 	// While there are customers in line or it's not the end of the day
 	while(current_time < NUM_SECONDS_IN_DAY || !isQueueEmpty()) {
 		// Access the queue
-		pthread_mutex_lock(&ml);
+		pthread_mutex_lock(&ml);								// Lock the mutex
 		if (isQueueEmpty()) {
-			pthread_mutex_unlock(&ml);
+			pthread_mutex_unlock(&ml);							// Unlock the mutex
 			// If the queue is empty, set waitStart
 			if (waitStart == 0) {
 				waitStart = current_time;
@@ -80,24 +77,23 @@ void *teller_runner(void *id) {
 		else {
 			// Did the teller have to wait?
 			if (waitStart != 0) {
-				int teller_wait = current_time - waitStart;	// How long the teller waited
-				total_teller_wait += teller_wait;	// Add to total teller wait time
+				int teller_wait = current_time - waitStart;			// How long the teller waited
+				total_teller_wait += teller_wait;					// Add to total teller wait time
 				if (teller_wait > max_teller_wait) {
-					max_teller_wait = teller_wait;	// New max teller wait time
+					max_teller_wait = teller_wait;					// New max teller wait time
 				}
 				number_of_teller_waits++;
 			}
 			// If the queue is not empty, take the first customer
 			Customer* c = dequeue();
-			pthread_mutex_unlock(&ml);
+			pthread_mutex_unlock(&ml);							// Unlock the mutex
 			total_customer_teller += c->transaction_time;
-			//printf("Got a customer for teller thread %ld\n", tid);
-			int customer_wait = current_time - c->arrival_time;	// How long the customer waited
-			total_customer_wait += customer_wait;	// Add to total customer wait time
+			int customer_wait = current_time - c->arrival_time;		// How long the customer waited
+			total_customer_wait += customer_wait;					// Add to total customer wait time
 			if (customer_wait > max_customer_wait) {
-				max_customer_wait = customer_wait;	// New max customer wait time
+				max_customer_wait = customer_wait;					// New max customer wait time
 			}
-			waitStart = 0;	// Reset the waitStart since we're done waiting
+			waitStart = 0;												// Reset the waitStart since we're done waiting
 			int end_transaction = current_time + c->transaction_time;	// When the transaction will be over
 			if (c->transaction_time > max_transaction_time) {
 				max_transaction_time = c->transaction_time;
@@ -106,10 +102,8 @@ void *teller_runner(void *id) {
 				;
 			}
 			total_customers_served++;
-			//printf("Teller thread %ld has finished a transaction\n", tid);
 		}
 	}
-	printf("Teller thread exit\n");
 	threads_running--;
 	pthread_exit(NULL);
 }
@@ -119,26 +113,18 @@ void *teller_runner(void *id) {
  * Customer thread
  */
 void *customer_queue_runner(void *id) {
-	printf("Customer queue thread started!\n");
 	int cust_index = 0;
 	// While there are generated customers to arrive
 	while(cust_index < total_customers_generated) {
 		Customer* c = cust_gen[cust_index];
-		//printf("Looking at customer %d - %d\n", c->arrival_time, current_time);
-		if (current_time % 1000 == 1) {
-			printf("Looking at customer %d - %d\n", c->arrival_time, current_time);
-		}
 		if (c->arrival_time <= current_time) {
-			//printf("Customer has arrived at %d\n", current_time);
 			// Next customer has arrived
-			pthread_mutex_lock(&ml);
+			pthread_mutex_lock(&ml);	// Lock mutex
 			enqueue(c);
-			//printQueue();
-			cust_index++;	// Move onto next customer to arrive
-			pthread_mutex_unlock(&ml);
+			cust_index++;				// Move onto next customer to arrive
+			pthread_mutex_unlock(&ml);	// Unlock mutex
 		}
 	}
-	printf("Customer Queue Thread exit\n");
 	threads_running--;
 	pthread_exit(NULL);
 }
@@ -148,25 +134,17 @@ void *customer_queue_runner(void *id) {
  * Timer thread
  */
 void *timer_thread_runner(void *id) {
-	printf("Timer thread started!\n");
 	int run = 1;
 	while (run) {
 		struct timespec ts;
 		ts.tv_sec = 0;
-		ts.tv_nsec = 100;
+		ts.tv_nsec = TIMER_INTERVAL_IN_NSEC;
 		nanosleep(&ts, NULL);
-		current_time+= 10;
-		if (current_time == NUM_SECONDS_IN_DAY) {
-			printf("EOD\n");
-		}
-		if (current_time % 1000 == 0) {
-			printf("Time: %d  Customers served: %d\n",current_time, total_customers_served);
-		}
+		current_time+= TIMER_COUNT;
 		if (threads_running == 0) {
 			run = 0;
 		}
 	}
-	printf("Timer Thread exit\n");
 	pthread_exit(NULL);
 }
 
@@ -177,10 +155,6 @@ int main(int argc, char *argv[]) {
 
 	// Generate customers
 	total_customers_generated = generateCustomers(cust_gen, NUM_SECONDS_IN_DAY);
-	printf("Generated %d customers\n", total_customers_generated);
-
-	// Initialize the mutex
-	// pthread_mutex_init(&ml, NULL);
 
 	// The start customer queue thread
 	pthread_create(&customer_queue_manager, NULL, customer_queue_runner, NULL);

c1eb7b0db61545d0a32401e30f20a9d1a9a1f3e8
amh3962 <amh3962@rit.edu>
2017-04-12 18:18:15 -0400

    Update timer.c

--- timer.c
+++ timer.c
@@ -1,6 +1,34 @@
 #include <time.h>
 #include <sys/siginfo.h>
-#include <sys/neutrino.h>
+#include <stdint.h>       /* for uintptr_t */
+#include <hw/inout.h>     /* for in*() and out*() functions */
+#include <sys/neutrino.h> /* for ThreadCtl() */
+#include <sys/syspage.h>  /* for for cycles_per_second */
+#include <sys/mman.h>     /* for mmap_device_io() */
+
+#define PORT_LENGTH 1
+#define CTRL_ADDRESS 0x28B
+#define DIOA_Address 0x288
+#define DIOB_Address 0x289
+#define CTRL_init 0x00 //DIO A & B as output
+
+uintptr_t ctrl_reg;
+uintptr_t portA;
+uintptr_t portB;
+
+timer_t PWM_A;
+timer_t PWM_B;
+struct sigevent PWM_A_Event;
+struct sigevent PWM_B_Event;
+
+void pin_init()
+{
+	ctrl_reg = mmap_device_io( PORT_LENGTH, CTRL_ADDRESS );
+	out8( ctrl_handle, CTRL_init);
+  
+  portA = mmap_device_io( PORT_LENGTH, DIOA_Address); 
+  portB = mmap_device_io( PORT_LENGTH, DIOB_Address);
+}
 
 void timer()
 {
@@ -33,8 +61,12 @@ void * int_thread (void)
     while (1)
     {
         InterruptWait (NULL, NULL);
-        
-        //TODO:Turn on output
+       
+        //Set A0 and B0 output high
+        out8(portA, 0x01);
+        out8(portB, 0x01);
+      
+        PWM_timers();
     }
 }
 
@@ -43,6 +75,8 @@ isr_handler (void, int id)
     // This causes the InterruptWait in "int_thread" to unblock.
     return (&event);
 }
+
+
 
 //ADD TO MAIN
   pthread_create (NULL, NULL, int_thread, NULL);

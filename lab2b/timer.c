a6f21f83d48567c6c483ab2984db8e3b3f53a262
amh3962 <amh3962@rit.edu>
2017-04-17 18:17:36 -0400

    Update timer.c

--- lab2b/timer.c
+++ lab2b/timer.c
@@ -1,52 +1,105 @@
 #include <time.h>
 #include <sys/siginfo.h>
-#include <sys/neutrino.h>
-#include "timer.h"
+#include <stdint.h>       /* for uintptr_t */
+#include <hw/inout.h>     /* for in*() and out*() functions */
+#include <sys/neutrino.h> /* for ThreadCtl() */
+#include <sys/syspage.h>  /* for for cycles_per_second */
+#include <sys/mman.h>     /* for mmap_device_io() */
+
+#define PORT_LENGTH 1
+#define CTRL_ADDRESS 0x28B
+#define DIOA_Address 0x288
+#define DIOB_Address 0x289
+#define CTRL_init 0x00 //DIO A & B as output
+
+uintptr_t ctrl_reg;
+uintptr_t portA;
+uintptr_t portB;
+
+timer_t             PWM_A; 
+struct sigevent     PWM_A_Event;
+struct itimerspec   timer_PWM_A;
 
 
+ timer_t             PWM_B;
+ struct sigevent     PWM_B_Event;
+ struct itimerspec   timer_PWM_B;
+
+void pin_init()
+{
+  ctrl_reg = mmap_device_io( PORT_LENGTH, CTRL_ADDRESS );
+  out8( ctrl_reg, CTRL_init);
+  
+  portA = mmap_device_io( PORT_LENGTH, DIOA_Address); 
+  portB = mmap_device_io( PORT_LENGTH, DIOB_Address);
+}
 
 void timer()
 {
-    timer_t             timerid;    // timer ID for timer
-    struct sigevent     event;      // event to deliver
-    struct itimerspec   timer;      // the timer data structure
+  timer_t             timer;    // timer ID for timer
+  struct sigevent     event;    // event to deliver
+  struct itimerspec   timer;    // the timer data structure
+        
+  SIGEV_INTR_INIT(&event);
+  SIGEV_INTR_INIT(&PWM_A_event);
+  SIGEV_INTR_INIT(&PWM_B_event);
   
-    SIGEV_INTR_INIT(&event);
+  timer_create (CLOCK_REALTIME, &event, &timerid);
+  timer_create (CLOCK_REALTIME, &PWM_A_event, &PWM_A);
+  timer_Create (CLOCK_REALTIME, &PWM_B_event, &PWM_B);
   
-    timer_create (CLOCK_REALTIME, &event, &timerid);
-  
-    // setup the timer (20ms delay, 20ms reload)
+  // setup the timer (20ms delay, 20ms reload)
     timer.it_value.tv_sec = 0;
     timer.it_value.tv_nsec = 20000000;
     timer.it_interval.tv_sec = 0;
     timer.it_interval.tv_nsec = 20000000;
     
-    // Start Timer
+  PWM_A_delay(1000000);
+  PWM_B_delay(1000000);
+        
+  // Start Timer
     timer_settime (timerid, 0, &timer, NULL);
+}
+
+void PWM_A_delay(int delay_A)
+{
+    // setup the timer (20ms delay, 20ms reload)
+    timer_PWM_A.it_value.tv_sec = 0;
+    timer_PWM_A.it_value.tv_nsec = delay_A;
+    timer_PWM_A.it_interval.tv_sec = 0;
+    timer_PWM_A.it_interval.tv_nsec = 0;
+}
+
+void PWM_B_delay(int delay_B)
+{
+    // setup the timer (20ms delay, 20ms reload)
+    timer_PWM_A.it_value.tv_sec = 0;
+    timer_PWM_A.it_value.tv_nsec = delay_B;
+    timer_PWM_A.it_interval.tv_sec = 0;
+    timer_PWM_A.it_interval.tv_nsec = 0;   
 }
 
 void * int_thread (void)
 {
-    // attach the ISR to IRQ 3
+    // attach the ISR to IRQ 0
     InterruptAttach (0, isr_handler, NULL, 0, 0);
-
-    ThreadCtl(_NTO_TCTL_IO, NULL);
 
     // now service the hardware when the ISR says to
     while (1)
     {
         InterruptWait (NULL, NULL);
-        // Turn both pins on
-        //out8(portA, 0x01);
-        //out8(portB, 0x01);
-        // If the PWM for either servos have changed...
-        // Wait the amount of time for
-        // Spawn a thread with new timer with same period as this (20ms)
+       
+        //Set A0 and B0 output high
+        out8(portA, 0x01);
+        out8(portB, 0x01);
+        
+        timer_settime (PWM_A, 0, &timer_PWM_A, NULL);
+        timer_settime (PWM_B, 0, &timer_PWM_B, NULL);
     }
 }
 
-void isr_handler (int id)
+event * isr_handler (void, int id)
 {
     // This causes the InterruptWait in "int_thread" to unblock.
-    //return (&event);
+    return (&event);
 }

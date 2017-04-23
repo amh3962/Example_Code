b36ffc89f5049bfd0fe092befc4c3ec8341930aa
Yura Kim <yuraxkim@gmail.com>
2017-04-23 00:16:49 -0400

    removed unused files

--- lab2b/console.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONSOLE_H_
-#define CONSOLE_H_
-
-
-void *input_check_runner(void *id);
-
-
-#endif /* CONSOLE_H_ */
--- lab2b/gpio.h
+++
@@ -1,8 +0,0 @@
-#ifndef GPIO_H_
-#define GPIO_H_
-
-
-
-
-
-#endif /* GPIO_H_ */

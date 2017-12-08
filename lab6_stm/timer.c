cc6f66c47724f9cc986f98b6ccbf90df02123f73
amh3962 <amh3962@rit.edu>
2017-12-08 14:57:48 -0500

    Cleanup comments

--- lab6_stm/timer.c
+++ lab6_stm/timer.c
@@ -11,24 +11,24 @@ void initTimer(void) {
 	RCC->APB2ENR |= 0x00000800;
   
 	//Timer 2 Setup -- Control servos
-	TIM2->PSC = 800;  				//Timer Prescaler = 800; 10 microseconds per count
+	TIM2->PSC = 800;  			//Timer Prescaler = 800; 10 microseconds per count
 	TIM2->EGR |= 0x0001; 			//Create update event
-	TIM2->ARR = 2000; 				//Set auto-load value; 2000 for 20 milliseconds
+	TIM2->ARR = 2000; 			//Set auto-load value; 2000 for 20 milliseconds
 	TIM2->CCER &= 0x0000;			//Turn off input enable
 	TIM2->CCMR1 &= 0x0000;
-	TIM2->CCMR1 |= 0x0068;  	//Input Capture pwm mode 1 for channel 1 
-	TIM2->CCER |= 0x0001; 		//Turn on output enable for channel 1 
+	TIM2->CCMR1 |= 0x0068;  		//Input Capture pwm mode 1 for channel 1 
+	TIM2->CCER |= 0x0001; 			//Turn on output enable for channel 1 
 	TIM2->CR1 &= 0x0000; 			//Reset control register
 	TIM2->CR1 |= 0x0080;			//Auto loading
 	
   //Timer 1 setup -- Wait Timer: Servo1
-	TIM1->PSC = 8000;  				//100us count
+	TIM1->PSC = 8000;  			//100us count
 	TIM1->EGR |= 0x0001; 			//Create update event
-	TIM1->ARR = 200; 					//Set auto-load value. 100 for 10 ms
-	TIM1->CCER &= 0x00000000; //Turn off input enable
-	TIM1->CCMR1 &= 0x0000; 		//Capture frozen. Using as Timing Base
+	TIM1->ARR = 200; 			//Set auto-load value. 100 for 10 ms
+	TIM1->CCER &= 0x00000000; 		//Turn off input enable
+	TIM1->CCMR1 &= 0x0000; 			//Capture frozen. Using as Timing Base
 	TIM1->CR1 &= 0x0000; 			//Reset control register
-	TIM1->CR1 |= 0x0010;		  //Set count direction. 0 = upcount, 1 = downcount
+	TIM1->CR1 |= 0x0010;		  	//Set count direction. 0 = upcount, 1 = downcount
 	TIM1->EGR |= 0x0001;
   
   //Enable timer

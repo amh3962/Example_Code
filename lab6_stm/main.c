e79fed14d581d87087b01e14d5a317c2b1c67291
amh3962 <amh3962@rit.edu>
2017-12-08 15:18:53 -0500

    Added project objective

--- lab6_stm/main.c
+++ lab6_stm/main.c
@@ -1,3 +1,15 @@
+/*
+
+Real Time and Embedded Systems - Lab6
+Yura Kim
+Aaron Halling
+
+Objective: Measure the output of a signal generator with a A/D converter using a QNX Neutrino program.
+Transfer the result to a STM32 development board to then convert the result into a servo posistion.
+*See Project6.pdf for complete description and requirements*
+
+*/
+
 #include "SysClock.h"
 #include "timer.h"
 #include "led.h"

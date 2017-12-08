1fd58f85c837fcde5609b761219fd28dbb30968c
amh3962 <amh3962@rit.edu>
2017-12-08 15:17:20 -0500

    Added project objective

--- lab6_qnx/lab6_qnx.c
+++ lab6_qnx/lab6_qnx.c
@@ -1,3 +1,13 @@
+/*
+Real Time and Embedded Systems - Lab6
+Yura Kim
+Aaron Halling
+
+Objective: Measure the output of a signal generator with a A/D converter using a QNX Neutrino program.
+Transfer the result to a STM32 development board to then convert the result into a servo posistion.
+
+*See Project6.pdf for complete description and requirements*
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include "ad_converter.h"
